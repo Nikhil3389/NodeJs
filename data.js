9bb04e56244ffff70f7c5523e66e9b20dd4ef2f7
Nikhil3389 <nikhil3389@gmail.com>
2023-05-27 23:02:54 +0530

    firstServer

---
+++ data.js
@@ -0,0 +1,9 @@
+const data = [
+  { name: "NIKHIL", email: "ABC@GMAIL.COM" },
+
+  { name: "NIKHIL", email: "ABC@GMAIL.COM" },
+  
+  { name: "NIKHIL", email: "ABC@GMAIL.COM" },
+];
+
+module.exports = data
--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const chalk  =  require('chalk');
-console.log(chalk.blue('Hello world'));
+// CREATING A SERVER FOR GET API
+
+const http =  require('http');
+const data =  require('./data');
+http.createServer((req,resp) =>{
+    resp.writeHead(200,{'content-type':'application/json'});
+    resp.write(JSON.stringify(data));
+    resp.end();
+}).listen(3000);
